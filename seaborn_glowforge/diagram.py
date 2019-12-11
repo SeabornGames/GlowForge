28062bb5c6a76f90a8adc80897d0f2e2000b8281
Ben Christenson <ben.christenson@xperi.com>
2019-12-10 22:48:25 -0600

    fixed width and height

--- seaborn_glowforge/diagram.py
+++ seaborn_glowforge/diagram.py
@@ -28,8 +28,8 @@ def main(cli_args=sys.argv[1:]):
 def create_diagram(args):
     grid = []
     for i in range(args.height):
-        if i%2:
-            row = (args.blank*4 + args.checker*4)*(args.width//2)
+        if i % 2:
+            row = (args.blank * 4 + args.checker * 4) * (args.width // 2)
         else:
             row = (args.checker * 4 + args.blank * 4) * (args.width // 2)
         grid += [row, row]
@@ -39,12 +39,13 @@ def create_diagram(args):
 def add_header(grid):
     width = len(grid[0]) // 4
     header = [' ' * 5] * 3
-    for w in range(width // 10):
-        header[0] += str(w % 10) * 40
-    header[1] += '0000111122223333444455556666777788889999' * (width // 10)
+    for w in range(width):
+        header[0] += str(w//10) + str(w//10) + str(w//10) + str(w//10)
+    for w in range(width):
+        header[1] += str(w%10) + str(w%10) + str(w%10) + str(w%10)
     header[2] += ' ¼½¾' * width
     for i, row in enumerate(grid):
-        grid[i] = str(i//2).rjust(4, ' ')+[' ', '½'][i%2] + grid[i]
+        grid[i] = str(i // 2).rjust(4, ' ') + [' ', '½'][i % 2] + grid[i]
     return header + grid
 
 
@@ -52,10 +53,9 @@ def parse_args(cli_args):
     parser = ArgumentParser(description='The seaborn_glowforge.diagram will'
                                         ' create a layout file or clean an'
                                         ' existing one.')
-    parser.add_argument('--width', type=int, default=100,
-                        help='number of feet per row in the file. round to the'
-                             'nearest ten.')
-    parser.add_argument('--height', type=int, default=100,
+    parser.add_argument('--width', type=int, default=None,
+                        help='number of feet per row in the file.')
+    parser.add_argument('--height', type=int, default=None,
                         help='number of feet of rows in the file.')
     parser.add_argument('--blank', default=' ',
                         help='character to use to represent blank space')
@@ -78,7 +78,11 @@ def parse_args(cli_args):
         args.output_file = args.input_file
     if args.output_file and args.backup_file is None:
         args.backup_file = f"{args.output_file.rsplit('.', 1)[0]}.bck"
-    args.width = args.width + (10 - args.width%10 if args.width%10 else 0)
+    if args.input_file is None:
+        if args.height is None:
+            args.height = 100
+        if args.width is None:
+            args.width = 100
     return args
 
 
@@ -92,19 +96,21 @@ class Cell:
         self.y = y
 
     def __repr__(self):
-        return '%s(%r, %r, %r)'%(self.__class__.__name__,
-                                 self.c, self.x, self.y)
+        return '%s(%r, %r, %r)' % (self.__class__.__name__,
+                                   self.c, self.x, self.y)
 
     @classmethod
     def parse_file(cls, args):
         with open(args.input_file, 'r') as fn:
             grid = fn.read().split('\n')
-        if grid[0].startswith('     000000'): # then strip header:
+        if grid[0].startswith('     000000'):  # then strip header:
             grid = [row[5:] for row in grid[3:]]
-        args.height = len(grid) // 2
-        args.width = len(grid[0]) // 4
-        for y, row in enumerate(grid):
-            for x, c in enumerate(row):
+        if args.height is None:
+            args.height = len(grid) // 2
+        if args.width is None:
+            args.width = len(grid[0]) // 4
+        for y, row in enumerate(grid[:args.height*2]):
+            for x, c in enumerate(row[:args.width*4]):
                 if c == args.blank or c == args.checker:
                     pass
                 elif c in Door.characters:
@@ -123,7 +129,7 @@ class Cell:
             v.clean()
         for k in list(cls.SPECIALS.keys()):
             v = cls.SPECIALS.get(k)
-            if v is not None: # specials are popped out with other specials
+            if v is not None:  # specials are popped out with other specials
                 v.clean(grid)
         for v in cls.GRID.values():
             row = list(grid[v.y])
@@ -133,46 +139,46 @@ class Cell:
     def clean(self):
         if (self.x, self.y + 1) in self.GRID:
             if (self.x, self.y - 1) in self.GRID:
-                if (self.x-1, self.y) in self.GRID:
-                    if (self.x+1, self.y) in self.GRID:
+                if (self.x - 1, self.y) in self.GRID:
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = self.internal
-                    else: # not right
+                    else:  # not right
                         self.c = self.right_intersect
-                else: # not left
-                    if (self.x+1, self.y) in self.GRID:
+                else:  # not left
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = self.left_intersect
-                    else: # not right or left
+                    else:  # not right or left
                         self.c = self.vertical
-            else: # not below
-                if (self.x-1, self.y) in self.GRID:
-                    if (self.x+1, self.y) in self.GRID:
+            else:  # not below
+                if (self.x - 1, self.y) in self.GRID:
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = self.bottom_intersect
-                    else: # not right
+                    else:  # not right
                         self.c = getattr(self, 'bottom_right_corner', self.c)
-                else: # not left
-                    if (self.x+1, self.y) in self.GRID:
+                else:  # not left
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = getattr(self, 'bottom_left_corner', self.c)
-                    else: # not right
+                    else:  # not right
                         pass
-        else: # not above
+        else:  # not above
             if (self.x, self.y - 1) in self.GRID:
-                if (self.x-1, self.y) in self.GRID:
-                    if (self.x+1, self.y) in self.GRID:
+                if (self.x - 1, self.y) in self.GRID:
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = self.top_intersect
-                    else: # not right
+                    else:  # not right
                         self.c = getattr(self, 'top_right_corner', self.c)
-                else: # not left
-                    if (self.x+1, self.y) in self.GRID:
+                else:  # not left
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = getattr(self, 'top_left_corner', self.c)
-                    else: # not right or left
+                    else:  # not right or left
                         pass
-            else: # not below
-                if (self.x-1, self.y) in self.GRID:
-                    if (self.x+1, self.y) in self.GRID:
+            else:  # not below
+                if (self.x - 1, self.y) in self.GRID:
+                    if (self.x + 1, self.y) in self.GRID:
                         self.c = self.horizontal
-                    else: # not right
+                    else:  # not right
                         pass
-                else: # not left
+                else:  # not left
                     pass
 
 
@@ -186,31 +192,33 @@ class Special(Cell):
         word = ''
         i = l = r = 0
         for i in range(100):
-            _next = self.SPECIALS.get((self.x+i, self.y), None)
+            _next = self.SPECIALS.get((self.x + i, self.y), None)
             if isinstance(_next, Special):
-                self.SPECIALS.pop((self.x+i, self.y))
+                self.SPECIALS.pop((self.x + i, self.y))
                 word += _next.c
-            elif (_next is None and (self.x+i, self.y) not in self.GRID and
-                      word[-1] != ' '):
+            elif (_next is None and (self.x + i, self.y) not in self.GRID and
+                          word[-1] != ' '):
                 word += ' '
-        for r in range(self.x+i, self.x+i+400):
+        for r in range(self.x + i, self.x + i + 400):
             if self.SPECIALS.get((r, self.y), None):
                 break
-        for l in range(self.x, self.x-400):
+        for l in range(self.x, self.x - 400):
             if l and self.SPECIALS.get((l, self.y), None):
                 break
         word = self.verticle_buffer + word.strip() + self.verticle_buffer
-        ljust = (r-l - len(word) // 2)
+        ljust = (r - l - len(word) // 2)
         row = grid[self.y]
-        grid[self.y] = row[:r+ljust] + word + row[r+len(word)+ljust*2:]
+        grid[self.y] = row[:r + ljust] + word + row[r + len(word) + ljust * 2:]
         # add buffer around word
-        word = self.horizontal_buffer*len(word)
+        word = self.horizontal_buffer * len(word)
         if self.y != 0:
             row = grid[self.y - 1]
-            grid[self.y-1] = row[:r+ljust] + word + row[r+len(word)+ljust*2:]
+            grid[self.y - 1] = row[:r + ljust] + word + row[r + len(
+                word) + ljust * 2:]
         if self.y != len(grid):
             row = grid[self.y + 1]
-            grid[self.y] = row[:r+ljust] + word + row[r+len(word)+ljust*2:]
+            grid[self.y] = row[:r + ljust] + word + row[
+                                                    r + len(word) + ljust * 2:]
 
 
 class Door(Cell):
@@ -252,9 +260,9 @@ class Window(Cell):
 class Wall(Cell):
     horizontal = '═'
     internal = '╬'
-    top_left_corner = '╒'
+    top_left_corner = '╔'
     top_intersect = '╦'
-    top_right_corner = '╕'
+    top_right_corner = '╗'
     bottom_left_corner = '╚'
     bottom_intersect = '╩'
     bottom_right_corner = '╝'
