d1a652dc8b6b5deb1a9a4e57286d048db916ba25
Ben Christenson <ben.christenson@xperi.com>
2020-03-03 20:24:21 -0600

    minor tweaks

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+setup(
+    name='seaborn-glowforge',
+    version='6.2.0',
+    description='SeabornGlowforge take a diagram (txt) file and draws the lines'
+                ' needed to cut the floors and walls out for making a model'
+                ' home.',
+    long_description=long_description,
+    author='Ben Christenson',
+    author_email='Python@BenChristenson.com',
+    url='https://github.com/SeabornGames/SeabornGlowforge',
+    install_requires=[
+        'seaborn_table',
+        'boxes',
+    ],
+    extras_require={
+    },
+    packages=[],
+    license='MIT License',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'),
+)
